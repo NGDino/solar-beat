372df189b02c8a68103a23dc9dafe982dcfb9840
Neil Dino <N.dino90@gmail.com>
2020-12-29 15:38:03 -0700

    popout menu basic layout

--- solar-beat/src/components/Header/index.js
+++ solar-beat/src/components/Header/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
+import {AppBar, Toolbar, Button,IconButton, Drawer, List, ListItem, ListItemText} from '@material-ui/core';
+
 import MenuIcon from '@material-ui/icons/Menu';
+
+//menu
+
+
+//solarbeat logo
+import Logo from '../../assets/solarbeat.png'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -17,24 +20,74 @@ const useStyles = makeStyles((theme) => ({
     title: {
         flexGrow: 1,
     },
+    list: {
+        width: 350,
+    },
+    fullList: {
+        width: 'auto',
+    },
+
+    
 }));
+
+
 
 export default function Header() {
     const classes = useStyles();
+
+    const [open, setOpen] = useState(false)
+    
+    const handleDrawerOpen = () => {
+        setOpen(true)
+    }
+
+    const handleDrawerClose = () => {
+        setOpen(false)
+    }
+
+    
 
     return (
         <div className={classes.root}>
         <AppBar position="static">
             <Toolbar>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" className={classes.title}>
-                Solar-Beat
-            </Typography>
-            <Button color="inherit">Savings Calculator</Button>
+
+                <IconButton
+                    edge="start" 
+                    className={classes.menuButton} 
+                    color="inherit" 
+                    onClick={handleDrawerOpen}
+                    aria-label="menu"
+                >
+                    <MenuIcon />
+                </IconButton>
+                
+                <div className={classes.title}>
+                    <img className="headLogo" src={Logo}/>
+                </div>
+                <Button 
+                    id="callToAction" 
+                    color="secondary"
+                    variant= "outlined"
+                >
+                    Savings Calculator
+                </Button>
             </Toolbar>
         </AppBar>
+
+        <Drawer anchor= 'left' open={open} onClose={handleDrawerClose}>
+            <List>
+                {['Home', 'Equipment', 'Solar Estimate', 'Contact Us'].map((text) => (
+                    <ListItem
+                        button key={text}
+                        onClick={handleDrawerClose}
+                        >
+                    <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+        </Drawer>
+            
         </div>
     );
 }
