527821f9e2cdc306a337f0c74a178d5e40f04085
Neil Dino <N.dino90@gmail.com>
2021-01-01 22:17:48 -0700

    started contact form

--- solar-beat/src/pages/Contact/index.js
+++ solar-beat/src/pages/Contact/index.js
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Box, FormControl, Input, FormHelperText, InputLabel, MenuItem, Select} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
+}));
 
 const Contact = () =>{
-    
-    return(
-        <h1>This is the Contact page</h1>
+    const classes = useStyles();
+    const [help, setHelp] = useState('');
 
+    const handleChange = (event) => {
+        setHelp(event.target.value);
+        console.log('help', help)
+    };
+
+    return(
+        <Box>
+            <h1>How can we Help you</h1>
+            <FormControl className={classes.formControl}>
+                <InputLabel id="how-help-label">I would like...</InputLabel>
+                <Select
+                    labelId="how-help-label"
+                    id="how-help"
+                    value={help}
+                    onChange={handleChange}
+                    >
+                        <MenuItem value={'info'}>I'd like more information</MenuItem>
+                        <MenuItem value={'quote'}>Get a Quote</MenuItem>
+                </Select>
+            </FormControl>
+        </Box>
+        
+
+        
     )
 }
 export default Contact;
